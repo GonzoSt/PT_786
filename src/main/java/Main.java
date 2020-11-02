93679c91991921e1daf121e02ecb9be74fe5353b
GonzoSt <zzposte@yandex.ru>
2020-11-02 23:17:19 +0500

    Tg_Task_3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,14 @@ public class Main {
     public static void main(String[] args) {
         int a = 1;
         System.out.println(a);
+
+        double b = 5.5;
+        System.out.println(b);
+
+        String c = "Hello";
+        System.out.println(c);
+
+        boolean d = true;
+        System.out.println(d);
     }
 }
