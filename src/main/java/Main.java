9943b3f57c5a771f060c47c698c9d584d46e3e3f
GonzoSt <zzposte@yandex.ru>
2020-11-02 23:14:50 +0500

    Tg_Task_4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        int a = 1;
-        System.out.println(a);
+        byte a = 1;
+        int b = a;
+
+        System.out.println(b);
+
     }
 }
