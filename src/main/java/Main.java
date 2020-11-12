31b09c47e90935683bec655504cb8c4e4d886d16
GonzoSt <zzposte@yandex.ru>
2020-11-12 18:38:21 +0500

    Task_3_Phone_г

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,35 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        Phone Phone_1 = new Phone();
-        Phone_1.number = "8-982-482-17-19";
-        Phone_1.model = "Samsung A30s";
-        Phone_1.weight = 50;
 
+        Phone Phone_1 = new Phone();
         System.out.println("Phone 1:");
-        System.out.println("Number: " + Phone_1.number);
-        System.out.println("Model: " + Phone_1.model);
-        System.out.println("Weight: " + Phone_1.weight);
+        Phone.receiveCall("Igor");
+        Phone.getNumber("8-919-118-01-18");
         System.out.println(" ");
 
         Phone Phone_2 = new Phone();
-        Phone_2.number = "8-999-999-99-99";
-        Phone_2.model = "Iphone XR";
-        Phone_2.weight = 100;
-
         System.out.println("Phone 2:");
-        System.out.println("Number: " + Phone_2.number);
-        System.out.println("Model: " + Phone_2.model);
-        System.out.println("Weight: " + Phone_2.weight);
+        Phone.receiveCall("Work");
+        Phone.getNumber("8-992-555-11-11");
         System.out.println(" ");
 
         Phone Phone_3 = new Phone();
-        Phone_3.number = "8-902-102-10-10";
-        Phone_3.model = "Xiaomi Redmi Note 6";
-        Phone_3.weight = 150;
-
         System.out.println("Phone 3:");
-        System.out.println("Number: " + Phone_3.number);
-        System.out.println("Model: " + Phone_3.model);
-        System.out.println("Weight: " + Phone_3.weight);
+        Phone.receiveCall("Mom");
+        Phone.getNumber("8-964-444-44-44");
     }
 }
