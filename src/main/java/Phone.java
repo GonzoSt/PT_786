e3ec023bd78b4cdbb825655a0cc7825109816f1a
GonzoSt <zzposte@yandex.ru>
2020-11-12 18:38:21 +0500

    Task_3_Phone_г

--- src/main/java/Phone.java
+++ src/main/java/Phone.java
@@ -3,4 +3,12 @@ public class Phone {
     public String number;
     public int weight;
     public String model;
+
+    public static void receiveCall (String callerName) {
+        System.out.println("Звонит " + callerName);
+    }
+
+    public static void getNumber (String callerNumber) {
+        System.out.println(callerNumber);
+    }
 }
