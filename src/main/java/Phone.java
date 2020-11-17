1c388a52d3f4da97d521382d2f6fca536fa58ca7
GonzoSt <zzposte@yandex.ru>
2020-11-17 20:31:42 +0500

    Task_3_Phone_и

--- src/main/java/Phone.java
+++ src/main/java/Phone.java
@@ -4,29 +4,31 @@ public class Phone {
     public int weight;
     public String model;
 
-//    public static void receiveCall (String callerName) {
-//        System.out.println("Звонит " + callerName);
-//    }
-//
+    public static void receiveCall (String callerName) {
+        System.out.println("Звонит " + callerName);
+    }
+
+    public static void receiveCall (String callerName, String callerNumber) {
+        System.out.println ("Звонит " + callerName + "\n" + "Номер: " + callerNumber);
+    }
+
 //    public static void getNumber (String callerNumber) {
 //        System.out.println(callerNumber);
 //    }
 
-    public Phone (String number, int weight, String model) {
-       this (number, model);
-       this.weight = weight;
-    }
-
-    public Phone (String number, String model){
-        this.number = number;
-        this.model = model;
-
-    }
-
-    public Phone () {
-        number = "RndmNumber";
-        weight = 100;
-        model = "RndmModel";
-
-    }
+//    public Phone (String number, int weight, String model) {
+//       this (number, model);
+//       this.weight = weight;
+//    }
+//
+//    public Phone (String number, String model){
+//        this.number = number;
+//        this.model = model;
+//    }
+//
+//    public Phone () {
+//        number = "RndmNumber";
+//        weight = 100;
+//        model = "RndmModel";
+//    }
 }
